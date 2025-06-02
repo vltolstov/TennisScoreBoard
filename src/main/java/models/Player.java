1930759e6423bdfb699a4ef800d657cd4ef0e549
Vladimir Tolstov <tolstov@gmail.com>
2025-06-02 15:07:51 +0700

    added player model

---
+++ src/main/java/models/Player.java
@@ -0,0 +1,25 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "players")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+}
