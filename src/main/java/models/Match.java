79dea78b9f20f44879c3fc34d967e7f7af5e02ee
Vladimir Tolstov <tolstov@gmail.com>
2025-06-02 15:08:02 +0700

    added match model

---
+++ src/main/java/models/Match.java
@@ -0,0 +1,32 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "matches")
+public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "player1")
+    @ManyToOne
+    @JoinColumn(name = "player1", referencedColumnName = "id")
+    private Player playerOne;
+
+    @Column(name = "player2")
+    @ManyToOne
+    @JoinColumn(name = "player2", referencedColumnName = "id")
+    private Player playerTwo;
+
+    @ManyToOne
+    @JoinColumn(name = "winner", referencedColumnName = "id")
+    private Player winner;
+}
