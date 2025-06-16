42a8b7a5ae7d9952efaa0eafd53aaa4ba267ba5b
Vladimir Tolstov <tolstov@gmail.com>
2025-06-16 15:39:02 +0700

    added exception handling filter and catch player name exception

---
+++ src/main/java/filters/ExceptionHandlingFilter.java
@@ -0,0 +1,26 @@
+package filters;
+
+import exceptions.PlayerNameException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class ExceptionHandlingFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+            super.doFilter(request, response, chain);
+        }
+        catch (PlayerNameException e){
+            request.setAttribute("error", e.getMessage());
+            request.getRequestDispatcher("new-match.jsp").forward(request, response);
+        }
+    }
+}
