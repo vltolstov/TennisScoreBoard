1d4ae0011b85aaa9511ee779e56d95f6432e09d2
Vladimir Tolstov <tolstov@gmail.com>
2025-06-16 15:36:07 +0700

    added NewMatchRequestDto

---
+++ src/main/java/dto/NewMatchRequestDto.java
@@ -0,0 +1,16 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewMatchRequestDto {
+
+    private String playerOneName;
+
+    private String playerTwoName;
+
+}
