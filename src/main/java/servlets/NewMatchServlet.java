503a07c7eb2f5571b3015919ff3c665eb97f0c6b
Vladimir Tolstov <tolstov@gmail.com>
2025-06-16 15:41:01 +0700

    added validating players name

--- src/main/java/servlets/NewMatchServlet.java
+++ src/main/java/servlets/NewMatchServlet.java
@@ -1,10 +1,12 @@
 package servlets;
 
+import dto.NewMatchRequestDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import utils.ValidationUtils;
 
 import java.io.IOException;
 
@@ -14,5 +16,21 @@ public class NewMatchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("new-match.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String playerOneName = request.getParameter("player-one-name");
+        String playerTwoName = request.getParameter("player-two-name");
+
+        NewMatchRequestDto newMatchRequestDto = new NewMatchRequestDto(playerOneName, playerTwoName);
+
+        ValidationUtils.validate(newMatchRequestDto);
+
+        //пишем базу
+        //создаем матч
+
+        response.sendRedirect("match-score.jsp");
     }
 }
