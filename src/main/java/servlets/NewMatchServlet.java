0a70728403d29e9d3870cf9c926e5cf335ef9447
Vladimir Tolstov <tolstov@gmail.com>
2025-06-18 23:32:48 +0700

    added save players to db for new match

--- src/main/java/servlets/NewMatchServlet.java
+++ src/main/java/servlets/NewMatchServlet.java
@@ -1,17 +1,25 @@
 package servlets;
 
+import dao.HibernatePlayerDao;
+import dao.PlayerDao;
 import dto.NewMatchRequestDto;
+import dto.PlayerDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import models.Player;
+import utils.MappingUtils;
 import utils.ValidationUtils;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/new-match")
 public class NewMatchServlet extends HttpServlet {
+
+    private final PlayerDao playerDao = new HibernatePlayerDao();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -28,8 +36,17 @@ public class NewMatchServlet extends HttpServlet {
 
         ValidationUtils.validate(newMatchRequestDto);
 
-        //пишем базу
-        //создаем матч
+        PlayerDto playerOneDto = new PlayerDto(playerOneName);
+        PlayerDto playerTwoDto = new PlayerDto(playerTwoName);
+
+        Player playerOne = playerDao.save(MappingUtils.convertToEntity(playerOneDto));
+        Player playerTwo = playerDao.save(MappingUtils.convertToEntity(playerTwoDto));
+
+
+
+//        List<Player> playerFromDb = playerDao.findAll();
+//        System.out.println(playerFromDb);
+//        создаем матч
 
         response.sendRedirect("match-score.jsp");
     }
