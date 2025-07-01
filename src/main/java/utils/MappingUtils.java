e8505f80062a5513e6a899f3357bea9f7e9e8881
Vladimir Tolstov <tolstov@gmail.com>
2025-07-01 20:13:09 +0700

    added MatchRequestDto and PlayerDto mappers

--- src/main/java/utils/MappingUtils.java
+++ src/main/java/utils/MappingUtils.java
@@ -1,6 +1,8 @@
 package utils;
 
+import dto.MatchRequestDto;
 import dto.PlayerDto;
+import models.Match;
 import models.Player;
 import org.modelmapper.ModelMapper;
 
@@ -10,10 +12,26 @@ public class MappingUtils {
 
     static {
         MODEL_MAPPER = new ModelMapper();
+
+        MODEL_MAPPER.typeMap(Match.class, MatchRequestDto.class).addMappings(mapper -> {
+            mapper.map(Match::getId, MatchRequestDto::setId);
+            mapper.map(src -> src.getFirstPlayer().getName(), MatchRequestDto::setFirstPlayerName);
+            mapper.map(src -> src.getSecondPlayer().getName(), MatchRequestDto::setSecondPlayerName);
+            mapper.map(src -> src.getScore().getSetsScore(), MatchRequestDto::setScore);
+            mapper.map(src-> src.getWinner().getName(), MatchRequestDto::setWinnerName);
+        });
     }
 
     public static Player convertToEntity(PlayerDto playerDto){
         return MODEL_MAPPER.map(playerDto, Player.class);
+    }
+
+    public static PlayerDto convertToDto(Player player){
+        return MODEL_MAPPER.map(player, PlayerDto.class);
+    }
+
+    public static MatchRequestDto convertToDto(Match match){
+        return MODEL_MAPPER.map(match, MatchRequestDto.class);
     }
 
 }
