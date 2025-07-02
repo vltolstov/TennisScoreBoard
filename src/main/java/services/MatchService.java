07e4dba54d7158bf345f0770d77d7c6e629d3288
Vladimir Tolstov <tolstov@gmail.com>
2025-07-02 19:12:26 +0700

    added game over checking

--- src/main/java/services/MatchService.java
+++ src/main/java/services/MatchService.java
@@ -5,6 +5,10 @@ import models.Score;
 
 public class MatchService {
 
+    public boolean checkGameOver(Match match) {
+        return match.getScore().getFirstPlayerSets() == 2 || match.getScore().getSecondPlayerSets() == 2;
+    }
+
     public void setWinner(Score score, Match match) {
         if(score.getFirstPlayerSets() == 2) {
             match.setWinner(match.getFirstPlayer());
