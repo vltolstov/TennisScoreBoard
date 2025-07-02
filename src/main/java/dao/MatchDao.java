67e07f02a0c5d482ba5e5f4488961396525b4227
Vladimir Tolstov <tolstov@gmail.com>
2025-07-02 19:10:03 +0700

    added findByPlayer

--- src/main/java/dao/MatchDao.java
+++ src/main/java/dao/MatchDao.java
@@ -1,6 +1,12 @@
 package dao;
 
 import models.Match;
+import models.Player;
+
+import java.util.List;
 
 public interface MatchDao extends CrudDao<Match, Integer> {
+
+    List<Match> findByPlayer(Player player);
+
 }
