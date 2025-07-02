cb566ffabfee19d8c920c6cb5296d608f314a700
Vladimir Tolstov <tolstov@gmail.com>
2025-07-02 19:12:04 +0700

    added match score calculate and set winner

--- src/main/java/servlets/MatchScoreServlet.java
+++ src/main/java/servlets/MatchScoreServlet.java
@@ -1,22 +1,34 @@
 package servlets;
 
+import dao.HibernateMatchDao;
+import dao.MatchDao;
+import dto.MatchRequestDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import models.Match;
+import models.Player;
 import models.Score;
+import org.modelmapper.ModelMapper;
 import services.MatchScoreService;
+import services.MatchService;
 import services.OngoingMatchesService;
+import utils.MappingUtils;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
+
+import static utils.MappingUtils.convertToDto;
 
 @WebServlet("/match-score")
 public class MatchScoreServlet extends HttpServlet {
 
     private final MatchScoreService matchScoreService = new MatchScoreService();
+    private final MatchService matchService = new MatchService();
+    private final MatchDao matchDao = new HibernateMatchDao();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -39,16 +51,24 @@ public class MatchScoreServlet extends HttpServlet {
         request.setAttribute("uuid", uuid.toString());
         request.setAttribute("match", match);
 
-        String pointWinnerId = request.getParameter("point-winner-id");
+        int pointWinnerId = Integer.parseInt(request.getParameter("point-winner-id"));
         Score score = match.getScore();
+        matchScoreService.scoreCalculate(score, match.getFirstPlayer().getId(), match.getSecondPlayer().getId(), pointWinnerId);
 
-        if(Integer.parseInt(pointWinnerId) == match.getFirstPlayer().getId()) {
-            score.setFirstPlayerPoints(score.getFirstPlayerPoints() + 1);
-        } else {
-            score.setSecondPlayerPoints(score.getSecondPlayerPoints() + 1);
+        if(matchService.checkGameOver(match)){
+
+            //OngoingMatchesService.removeMatch(uuid);
+            matchService.setWinner(score, match);
+            matchDao.save(match);
+
+            MatchRequestDto matchRequestDto = convertToDto(match);
+            request.setAttribute("firstPlayerName", matchRequestDto.getFirstPlayerName());
+            request.setAttribute("secondPlayerName", matchRequestDto.getSecondPlayerName());
+            request.setAttribute("score", matchRequestDto.getScore());
+            request.setAttribute("winnerName", matchRequestDto.getWinnerName());
+            request.getRequestDispatcher("winner.jsp").forward(request, response);
         }
 
         request.getRequestDispatcher("match-score.jsp").forward(request, response);
-
     }
 }
