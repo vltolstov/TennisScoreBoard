3dcbb05d34001a54eee121f0cc3f394e6ddad9fa
Vladimir Tolstov <tolstov@gmail.com>
2025-07-07 20:20:46 +0700

    added doPost and fixed doGet for filter by player name

--- src/main/java/servlets/MatchesServlet.java
+++ src/main/java/servlets/MatchesServlet.java
@@ -1,48 +1,63 @@
 package servlets;
 
 import dao.HibernateMatchDao;
+import dao.HibernatePlayerDao;
 import dao.MatchDao;
+import dao.PlayerDao;
+import exceptions.PlayerNameException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import models.Match;
+import models.Player;
 import utils.ValidationUtils;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @WebServlet("/matches")
 public class MatchesServlet extends HttpServlet {
 
     private static final int DEFAULT_PAGE_NUMBER = 1;
     private final MatchDao matchDao = new HibernateMatchDao();
+    private final PlayerDao playerDao = new HibernatePlayerDao();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String page = request.getParameter("page");
-        String filterByPlayerName = request.getParameter("filter_by_player_name");
-        int pageNumber;
         List<Match> matches;
 
+        String page = request.getParameter("page");
+        int pageNumber;
+
         ValidationUtils.validate(page);
+
         if(page == null) {
             pageNumber = DEFAULT_PAGE_NUMBER;
         } else {
             pageNumber = Integer.parseInt(page);
         }
 
-        // + валидация имени игрока
-        if(filterByPlayerName == null) {
+        String playerName = request.getParameter("filter_by_player_name");
+        if(playerName == null) {
             matches = matchDao.findAll();
+        } else {
+            Optional<Player> optionalPlayer = Optional.empty();
+            optionalPlayer = playerDao.findByName(playerName);
+            Player player = optionalPlayer.orElseThrow(() -> new PlayerNameException("Player not found"));
+            matches = matchDao.findByPlayer(player);
         }
 
-        matches = matchDao.findAll();
-        //matches = matchDao.findByPlayer();
-
         request.setAttribute("matches", matches);
-
         request.getRequestDispatcher("matches.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String playerName = request.getParameter("filter_by_player_name");
+
+        response.sendRedirect("matches" + "?filter_by_player_name=" + playerName);
     }
 }
