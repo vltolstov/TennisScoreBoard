42a4863bd73c51d7bb172fad934af334a0b39a8e
Vladimir Tolstov <tolstov@gmail.com>
2025-07-14 11:08:22 +0700

    added pagination. optimized filter by player

--- src/main/java/servlets/MatchesServlet.java
+++ src/main/java/servlets/MatchesServlet.java
@@ -4,7 +4,7 @@ import dao.HibernateMatchDao;
 import dao.HibernatePlayerDao;
 import dao.MatchDao;
 import dao.PlayerDao;
-import exceptions.PlayerNameException;
+import dto.PaginationResultDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,23 +12,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import models.Match;
 import models.Player;
+import services.MatchPaginationService;
 import utils.ValidationUtils;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 @WebServlet("/matches")
 public class MatchesServlet extends HttpServlet {
 
     private static final int DEFAULT_PAGE_NUMBER = 1;
+    private static final int DEFAULT_PAGE_SIZE = 10;
     private final MatchDao matchDao = new HibernateMatchDao();
     private final PlayerDao playerDao = new HibernatePlayerDao();
+    private final MatchPaginationService matchPaginationService = new MatchPaginationService(matchDao);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        List<Match> matches;
 
         String page = request.getParameter("page");
         int pageNumber;
@@ -42,17 +43,21 @@ public class MatchesServlet extends HttpServlet {
         }
 
         String playerName = request.getParameter("filter-by-player-name");
+
+        PaginationResultDto<Match> pageResult;
         if(playerName == null) {
-            matches = matchDao.findAll();
+            pageResult = matchPaginationService.getMatchesPage(pageNumber, DEFAULT_PAGE_SIZE);
         } else {
-            Optional<Player> player = Optional.empty();
-            player = playerDao.findByName(playerName);
-            matches = player
-                    .map(matchDao::findByPlayer)
-                    .orElse(Collections.emptyList());
+            Optional<Player> optionalPlayer = playerDao.findByName(playerName);
+                pageResult = optionalPlayer
+                        .map(player -> matchPaginationService.getMatchesPage(pageNumber, DEFAULT_PAGE_SIZE, player))
+                        .orElseGet(() -> new PaginationResultDto<>(Collections.emptyList(), pageNumber, 0));
         }
 
-        request.setAttribute("matches", matches);
+        request.setAttribute("matches", pageResult.getResults());
+        request.setAttribute("currentPage", pageResult.getPage());
+        request.setAttribute("totalPages", pageResult.getTotalPages());
+
         request.getRequestDispatcher("matches.jsp").forward(request, response);
     }
 
